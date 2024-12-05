136356da4213d805a593e249bb3aa47b85088fa1
Luhcyy <super.ghast8@gmail.com>
2024-12-05 10:50:07 -0300

    Fix: now superUsers can be created normally on django admin

--- CRUD/core/models.py
+++ CRUD/core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from core.managers import CustomUserManager
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=255)
@@ -18,6 +20,8 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['full_name']
+    
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
